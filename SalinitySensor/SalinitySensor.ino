221392a3001b24fd714d8cfd86dd29b3ba7d0c77
Joshwa Moellenkamp <jmolecavalier@gmail.com>
2014-11-30 11:43:14 -0700

    Solenoid and fan control 100% complete. Methods now print behavior, indicating what is going on under the hood.

--- SalinitySensor/SalinitySensor.ino
+++ SalinitySensor/SalinitySensor.ino
@@ -5,64 +5,100 @@
 // Include the library dependency for the servo
 #include <Servo.h> 
 
+// Initialize the tempurature sensor.
 dht DHT;
 
 // Digital Pins
 int DHT11_PIN   = 6; // PIN that will read the signal from the Tempurature Sensor
-int mixingPin   = 3; // PIN that will write a digital LOW to trigger the relay and turn on the mixing fan
 int solenoidPin = 5; // PIN that will write a digital LOW to trigger the relay and turn on the solenoid
+int mixingPin   = 3; // PIN that will write a digital LOW to trigger the relay and turn on the mixing fan
 
 // Analog Pins
 int salinityPin = 5; // PIN that will read the signal from the Salinity Sensor
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// USER DECLARATIONS DETERMINING THE FUNCTIONALITY OF THE PROGRAM GO HERE ///////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+int reservoirTime = 4000; // The amount of time that the solenoid will open the reservoir for.
+int mixTime       = 8000; // The amount of time that the mixing plate will spin for.
+int hopperTime    = 4000; // The amount of time that the servo will leave the hopper open for.
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Setup method for the program.
 void setup()
 {
   Serial.begin(9600);
-  // Set the pin mode for the mixingPin to be an output.
+  
+  // Set the pin mode for the relay pins.
   pinMode(mixingPin, OUTPUT);
-  // Print the headers for the serial monitor.
-  Serial.println("Humidity (%)\tTempurature (C)\t\tSalinity (V)");
+  pinMode(solenoidPin, OUTPUT);
+  
+  // Behavior for the output pins will be random unless explicitly set here.
+  digitalWrite(mixingPin, HIGH);
+  digitalWrite(solenoidPin, HIGH);
+  
   // Call the dumpReservoir() method.
   dumpReservoir();
+  
   // Call the mix() method to agitate the mixture.
   mix();
+  
   // Call the dumpBeads() method to add the beads.
   dumpBeads();
+ 
+  // Print the headers for the serial monitor.
+  Serial.println("Humidity (%)\tTempurature (C)\t\tSalinity (V)");
 }
 
-// Dump the solution for a variable amount of time to get the desired solution. 
+// Method that will dump the solution.
 void dumpReservoir()
 {
-  Serial.println("dumpReservoir() function entered");
-  // TODO: FLIP THIS FOR ACTUAL USE.
-  digitalWrite(solenoidPin, HIGH);
-  delay(5000);
-  // TODO: FLIP THIS FOR ACTUAL USE.
+  Serial.print("Dumping reservoir for ");
+  Serial.print(reservoirTime/1000);
+  Serial.print(" seconds... ");
+  
+  // Activate the solenoid by grounding the input on the relay.
   digitalWrite(solenoidPin, LOW);
+  
+  // The following line will determine how long the solenoids will stay open.
+  delay(reservoirTime);
 
+  // Deactivate the solenoid as the process has been finished.
+  digitalWrite(solenoidPin, HIGH);
+  Serial.println("Finished.");
 }
 
 // Method that will turn on the mixer. 
-// Set the delay() instruction to determine how long it will be left on.
 void mix()
 {
+  Serial.print("Mixing solution for ");
+  Serial.print(mixTime/1000);
+  Serial.print(" seconds... ");
+  
   // Turn on the relay.
   digitalWrite(mixingPin, LOW); 
   
   // The following line will determine how long the mixer will run.
-  delay(10000);
+  delay(mixTime);
   
   // Turn off the relay.
   digitalWrite(mixingPin, HIGH);
+  Serial.println("Finished.");
 }
 
-// Dump the beads from the hopper.
-// Set the delay() instruction to determine how long it will be left on.
+// Method that will activate the bead hopper.
 void dumpBeads()
 {
-  Serial.println("dumpBeads() function entered");
-
+  Serial.print("Opening bead hopper for ");
+  Serial.print(hopperTime/1000);
+  Serial.print(" seconds... ");
+  
+  Serial.println("Finished.");
 }
 
 // Check the salinity and ambient data readings every 2 seconds
